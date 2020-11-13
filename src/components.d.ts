843499c8f9295ac125c9fbb7cc9984b54144a421
Sergio <60603572+sergiogataca@users.noreply.github.com>
2020-11-13 11:36:02 +0000

    V1.1 (#1)
    * style update
    
    * style update
    
    * styles and props update
    
    * readme fixes and naming
    
    * readme fixes and naming
    
    * text fixes
    
    * get back the readme example
    
    * version and tabs
    
    * change version
    
    Co-authored-by: sergio gordo <sergormo@gmail.com>

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,13 +5,14 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { RESULT_STATUS, } from "./utils/utils";
+import { RESULT_STATUS } from "./utils/utils";
 export namespace Components {
     interface GatacaQr {
         /**
           * _[Optional]_ Decide if to show it as a button to display the QR Or display directly the QR. Default: true (display button)
          */
         "asButton": boolean;
+        "buttonText"?: string;
         /**
           * ***Mandatory*** Connect Server where the wallet will send the data
          */
@@ -19,7 +20,7 @@ export namespace Components {
         /**
           * _[Optional]_ Check status function to query the current status of the session If not set, it would fallback to the session Endpoint property.
          */
-        "checkStatus"?: (id?: string) => Promise<RESULT_STATUS>;
+        "checkStatus"?: (id ?: string) => Promise<RESULT_STATUS>;
         /**
           * _[Optional]_ Create session function to generate a new Session If the property is unset, it will fallback to the generation Endpoint property.
          */
@@ -35,7 +36,7 @@ export namespace Components {
         /**
           * ***Mandatory*** Callback fired upon session expired or invalid If not set, session error would not be handled An error containing information will be passed as parameter
          */
-        "errorCallback": (error?: Error) => void;
+        "errorCallback": (error ?:Error) => void;
         /**
           * _[Optional]_ Session Generation URL to create a new Session. It will expect to receive the session Id from the response header 'X-Connect-Id'. If not set, it would use a default endpoint to the same window URL under the path /auth
          */
@@ -52,6 +53,8 @@ export namespace Components {
           * _[Optional]_ Frequency in seconds to check if the session has been validated
          */
         "pollingFrequency"?: number;
+        "qrModalDescription"?: string;
+        "qrModalTitle"?: string;
         /**
           * _[Optional]_ Decide if scanning the credential as a verifier to request credentials or as an issuer too issue credentials. Options: scan (default) | credential
          */
@@ -75,7 +78,7 @@ export namespace Components {
         /**
           * ***Mandatory*** Callback fired upon session correctly verified If not set, session validation wouldn't trigger any action The session data and a possible token will be sent as parameters to the callback
          */
-        "successCallback": (data?: any, token?: string) => void;
+        "successCallback": (data ?: any, token ?: string) => void;
     }
 }
 declare global {
@@ -95,6 +98,7 @@ declare namespace LocalJSX {
           * _[Optional]_ Decide if to show it as a button to display the QR Or display directly the QR. Default: true (display button)
          */
         "asButton"?: boolean;
+        "buttonText"?: string;
         /**
           * ***Mandatory*** Connect Server where the wallet will send the data
          */
@@ -102,7 +106,7 @@ declare namespace LocalJSX {
         /**
           * _[Optional]_ Check status function to query the current status of the session If not set, it would fallback to the session Endpoint property.
          */
-        "checkStatus"?: (id?: string) => Promise<RESULT_STATUS>;
+        "checkStatus"?: (id ?: string) => Promise<RESULT_STATUS>;
         /**
           * _[Optional]_ Create session function to generate a new Session If the property is unset, it will fallback to the generation Endpoint property.
          */
@@ -114,7 +118,7 @@ declare namespace LocalJSX {
         /**
           * ***Mandatory*** Callback fired upon session expired or invalid If not set, session error would not be handled An error containing information will be passed as parameter
          */
-        "errorCallback"?: (error?: Error) => void;
+        "errorCallback"?: (error ?:Error) => void;
         /**
           * _[Optional]_ Session Generation URL to create a new Session. It will expect to receive the session Id from the response header 'X-Connect-Id'. If not set, it would use a default endpoint to the same window URL under the path /auth
          */
@@ -131,6 +135,8 @@ declare namespace LocalJSX {
           * _[Optional]_ Frequency in seconds to check if the session has been validated
          */
         "pollingFrequency"?: number;
+        "qrModalDescription"?: string;
+        "qrModalTitle"?: string;
         /**
           * _[Optional]_ Decide if scanning the credential as a verifier to request credentials or as an issuer too issue credentials. Options: scan (default) | credential
          */
@@ -150,7 +156,7 @@ declare namespace LocalJSX {
         /**
           * ***Mandatory*** Callback fired upon session correctly verified If not set, session validation wouldn't trigger any action The session data and a possible token will be sent as parameters to the callback
          */
-        "successCallback"?: (data?: any, token?: string) => void;
+        "successCallback"?: (data ?: any, token ?: string) => void;
     }
     interface IntrinsicElements {
         "gataca-qr": GatacaQr;
