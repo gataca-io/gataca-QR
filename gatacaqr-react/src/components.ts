6c1dddc0da6373332fb313d37b9f503b650b4b12
José San Juan <jose@gataca.io>
2020-11-16 11:30:33 +0100

    React build

--- gatacaqr-react/src/components.ts
+++ gatacaqr-react/src/components.ts
@@ -3,9 +3,8 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import { JSX } from 'gatacaqr';
+import type { JSX } from 'gatacaqr';
 
-import { defineCustomElements, applyPolyfills } from 'gatacaqr/loader';
 
-applyPolyfills().then(() => defineCustomElements());
+
 export const GatacaQr = /*@__PURE__*/createReactComponent<JSX.GatacaQr, HTMLGatacaQrElement>('gataca-qr');
