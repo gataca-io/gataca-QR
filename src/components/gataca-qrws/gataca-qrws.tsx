5e01d7cfbb0dd1dee54b9fc35e90e25cd955f603
angiebenavente <angela@gataca.io>
2024-04-04 12:23:16 +0200

    equalize(gataca-qrws) set equal components and styles than gataca-qr

--- src/components/gataca-qrws/gataca-qrws.tsx
+++ src/components/gataca-qrws/gataca-qrws.tsx
@@ -8,9 +8,6 @@ import {
   Prop,
   State,
 } from "@stencil/core";
-import alertIcon from "../../assets/icons/gat-icon-alert.svg";
-import successIcon from "../../assets/icons/gat-icon-check.svg";
-import refreshIcon from "../../assets/icons/gat-icon-refresh.svg";
 import logoGataca from "../../assets/images/logo_gataca.svg";
 import "../gataca-qrdisplay/gataca-qrdisplay";
 
@@ -20,6 +17,9 @@ import {
   RESULT_STATUS,
   WSResponse,
 } from "../../utils/utils";
+import { Success } from "./components/success/Success";
+import { RetryButton } from "./components/retryButton/RetryButton";
+import { QR } from "./components/qr/QR";
 
 const DEEP_LINK_PREFIX =
   "https://gataca.page.link/?apn=com.gatacaapp&ibi=com.gataca.wallet&link=";
@@ -125,16 +125,137 @@ export class GatacaQRWS {
 
   /**
    * _[Optional]_
+   * String to set Modal title color
+   */
+  @Prop() modalTitleColor?: string = "#4745B7";
+
+  /**
+   * _[Optional]_
    * Modifies the Modal description
    */
   @Prop() qrModalDescription?: string =
     "Sign up or sign in by scanning the QR Code with the Gataca Wallet";
+  /**
+   * _[Optional]_
+   * Boolean to show or not show the modal title, brandTitle and description
+   */
+  @Prop() hideModalTexts?: boolean = false;
+
+  /**
+   * _[Optional]_
+   * Boolean to show or not show the modal title, brandTitle and description
+   */
+  @Prop() hideModalBoxShadow?: boolean = false;
 
   /**
    * _[Optional]_
    * Boolean to show or not show the gataca brand title
    */
   @Prop() hideBrandTitle?: boolean = false;
+  /**
+   * _[Optional]_
+   * Size of the logo to display in percentage to the total size [0-1]. 0 means no logo will be displayed. Default is the GATACA logo. Recommended size is around 0.33
+   */
+  @Prop() logoSize?: number = 0;
+
+  /**
+   * _[Optional]_
+   * Logo to display, just if the logo size is greater than 0. No logo is the GATACA logo.
+   */
+  @Prop() logoSrc?: string = logoGataca;
+
+  /**
+   * _[Optional]_
+   * Size of QR Displayed
+   */
+  @Prop() qrSize?: number = 300;
+
+  /**
+   * _[Optional]_
+   * Width of the modal
+   */
+  @Prop() modalWidth?: number = 300;
+
+  /**
+   * _[Optional]_
+   * Height of the modal
+   */
+  @Prop() modalHeight?: number;
+
+  /**
+   * _[Optional]_
+   * String to show when qr code expired
+   */
+  @Prop() qrCodeExpiredLabel?: string = "QR Code expired";
+
+  /**
+   * _[Optional]_
+   * String to show when credentials not validatedd
+   */
+  @Prop() credentialsNotValidatedLabel?: string =
+    "User credentials not validated";
+
+  /**
+   * _[Optional]_
+   * String to show "click inside" label
+   */
+  @Prop() clickInsideBoxLabel?: string = "Click inside the box to";
+
+  /**
+   * _[Optional]_
+   * String to show "refresh QR" label
+   */
+  @Prop() refreshQrLabel?: string = "Refresh QR Code";
+
+  /**
+   * _[Optional]_
+   * String to show "scan QR" label
+   */
+  @Prop() scanQrLabel?: string = "Scan QR Code";
+
+  /**
+   * _[Optional]_
+   * String to show "user not scan in time" error
+   */
+  @Prop() userNotScanInTimeErrorLabel?: string =
+    "User did not scan the QR in the allowed time";
+
+  /**
+   * _[Optional]_
+   * String to show "provided credentials not validates" error
+   */
+  @Prop() credsNotValidatedErrorLabel?: string =
+    "Provided user credentials couldn't be validated";
+
+  /**
+   * _[Optional]_
+   * String to show "failed login" error
+   */
+  @Prop() failedLoginErrorLabel?: string = "No successful login";
+
+  /**
+   * _[Optional]_
+   * String to show "successful login" label
+   */
+  @Prop() successLoginLabel?: string = "Successful Connection!";
+
+  /**
+   * _[Optional]_
+   * String to show "by brand" label
+   */
+  @Prop() byBrandLabel?: string = "by Gataca";
+
+  /**
+   * _[Optional]_
+   * String to show "waiting start session" label
+   */
+  @Prop() waitingStartSessionLabel?: string = "waiting to start a session";
+
+  /**
+   * _[Optional]_
+   * Boolean to show or not show the QR Modal description
+   */
+  @Prop() hideQrModalDescription?: boolean = false;
 
   /**
    * _[Optional]_
@@ -311,11 +432,11 @@ export class GatacaQRWS {
       case RESULT_STATUS.NOT_STARTED:
         return this.renderRetryButton();
       case RESULT_STATUS.ONGOING:
-        return this.renderQR(this.getLink(), 300, true);
+        return this.renderQR(this.getLink(), true);
       case RESULT_STATUS.EXPIRED:
-        return this.renderRetryButton("QR Code expired");
+        return this.renderRetryButton(this.qrCodeExpiredLabel);
       case RESULT_STATUS.FAILED:
-        return this.renderRetryButton("User credentials not validated");
+        return this.renderRetryButton(this.credentialsNotValidatedLabel);
       case RESULT_STATUS.SUCCESS:
         return this.renderSuccess();
     }
@@ -323,45 +444,46 @@ export class GatacaQRWS {
 
   renderSuccess() {
     return (
-      <div class="success">
-        <img src={successIcon} height={52} width={52}></img>
-        <p class="successMsg">Successful Connection!</p>
-      </div>
+      <Success
+        modalHeight={this?.modalHeight}
+        successLoginLabel={this?.successLoginLabel}
+      />
     );
   }
 
   renderRetryButton(errorMessage?: string) {
     return (
-      <div class="reload">
-        <div id="notify" onClick={() => this.display()}>
-          <img src={refreshIcon} height={24} width={24} />
-          <p class="qrDescription">Click inside the box to </p>
-          {errorMessage ? (
-            <p class="qrDescription bold">Refresh QR Code</p>
-          ) : (
-            <p class="qrDescription bold">Scan QR Code</p>
-          )}
-          {errorMessage && (
-            <div class="alert">
-              <img src={alertIcon} height={24} width={24}></img>
-              <p>{errorMessage}</p>
-            </div>
-          )}
-        </div>
-        <div id="qrwait">
-          {this.renderQR("waiting to start a session", 250)}
-        </div>
-      </div>
+      <RetryButton
+        errorMessage={errorMessage}
+        modalWidth={this?.modalWidth}
+        clickInsideBoxLabel={this?.clickInsideBoxLabel}
+        refreshQrLabel={this?.refreshQrLabel}
+        scanQrLabel={this?.scanQrLabel}
+        waitingStartSessionLabel={this?.waitingStartSessionLabel}
+        display={this?.display}
+        renderRetryQR={this?.renderRetryQR}
+      />
     );
   }
 
-  renderQR(value: string, size: number, useLogo?: boolean) {
+  renderQR(value: string, useLogo?: boolean) {
     return (
-      <gataca-qrdisplay
-        qrData={value}
-        rounded={true}
-        size={size}
-        logo-size={useLogo ? 0.33 : 0}
+      <QR
+        value={value}
+        useLogo={useLogo}
+        size={this?.qrSize || undefined}
+        logoSrc={this?.logoSrc}
+      />
+    );
+  }
+
+  renderRetryQR(value: string, useLogo?: boolean) {
+    return (
+      <QR
+        value={value}
+        useLogo={useLogo}
+        size={this?.qrSize ? this?.qrSize - 50 : undefined}
+        logoSrc={this?.logoSrc}
       />
     );
   }
@@ -370,27 +492,57 @@ export class GatacaQRWS {
     return (
       <div class="popUpContainer">
         <div
-          class={"modal-window is-visible"}
+          class={`is-visible modal-window ${
+            this.hideModalTexts ? "" : "large-modal"
+          } ${this.hideModalBoxShadow ? "noBoxShadow" : ""}`}
+          style={{
+            width: (this.modalWidth - 2).toString() + "px",
+            height: this.modalHeight
+              ? (this.modalHeight - 2)?.toString() + "px"
+              : "",
+          }}
           onClick={(event) => {
             event.stopPropagation();
           }}
         >
           <div class="modal-window__content">
-            <div class="qrTitleContainer">
-              <p class="qrTitle">{this.qrModalTitle}</p>
+            <div
+              class={`qrTitleContainer ${
+                this.hideModalTexts ? "hidenText" : ""
+              }`}
+            >
+              <p
+                class="qrTitle modalText"
+                style={{ color: this.modalTitleColor || "#1e1e20" }}
+              >
+                {this.qrModalTitle}
+              </p>
               {!this.hideBrandTitle && (
-                <p class="qrBrand">
-                  by Gataca{" "}
+                <p class="qrBrand modalText">
+                  {this.byBrandLabel}{" "}
                   <span>
                     <img src={logoGataca} />
                   </span>
                 </p>
               )}
-              {this.result !== RESULT_STATUS.SUCCESS && (
-                <p class="qrDescription">{this.qrModalDescription}</p>
-              )}
+              {this.result !== RESULT_STATUS.SUCCESS &&
+                !this.hideQrModalDescription && (
+                  <p class="qrDescription modalText">
+                    {this.qrModalDescription}
+                  </p>
+                )}
             </div>
-            <div class="qrSection">{this.renderQRSection()}</div>
+            <div
+              class="qrSection"
+              style={{
+                width: this.modalWidth.toString() + "px",
+                height: this.modalWidth
+                  ? this.modalWidth?.toString() + "px"
+                  : "",
+              }}
+            >
+              {this.renderQRSection()}
+            </div>
           </div>
         </div>
       </div>
